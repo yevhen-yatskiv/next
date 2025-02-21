929f3258194426345e66d2365f87520a10bd9652
Yevhen <yevhen.yatskiv@gmail.com>
2025-02-21 16:21:48 +0100

    Added carousel component

--- crypto-tracker/app/page.tsx
+++ crypto-tracker/app/page.tsx
@@ -1,9 +1,11 @@
+import Carousel from "./components/Carousel";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Carousel />
     </div>
   );
 }
